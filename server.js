b4307d32a9aedf6866e803c5eb861f78cfb21fd0
mptmadhushan <tharindumadhushan420@outlook.com>
2020-10-19 02:12:15 +0530

    make it better

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ require("./app/routes/subCategory.routes")(app);
 require("./app/routes/products.routes")(app);
 require("./app/routes/insight.routes")(app);
 // set port, listen for requests
-app.listen(3001, () => {
+app.listen || 5000, () => {
   console.log("Server is running on port 3001.");
-});
+}
