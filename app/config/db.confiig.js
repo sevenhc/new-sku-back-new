26408d766b31c34208cbb331a4c749edc5d32d40
mptmadhushan <tharindumadhushan420@outlook.com>
2020-10-19 21:18:42 +0530

    added fixes

--- app/config/db.confiig.js
+++ app/config/db.confiig.js
@@ -1,12 +1,18 @@
+module.exports = {
+  HOST: "us-cdbr-east-02.cleardb.com",
+  USER: "b75cf9bd67edd1",
+  PASSWORD: "10863cba",
+  DB: "heroku_f1e35368948d17d",
+};
 // module.exports = {
 //   HOST: "localhost",
 //   USER: "root",
 //   PASSWORD: "password",
 //   DB: "newSku",
 // };
-module.exports = {
-  HOST: "sql12.freemysqlhosting.net",
-  USER: "sql12370395",
-  PASSWORD: "SI1NeBiVWg",
-  DB: "sql12370395",
-};
+// module.exports = {
+//   HOST: "sql12.freemysqlhosting.net",
+//   USER: "sql12370395",
+//   PASSWORD: "SI1NeBiVWg",
+//   DB: "sql12370395",
+// };
