32c6e78d72255430374cbb13e751b122531d633e
SEVENHC <hasitha.chamikara@sevenhc.net>
2021-01-26 01:19:14 +0530

    Email Template Test

--- app/controllers/subscription.mail.js
+++ app/controllers/subscription.mail.js
@@ -3,7 +3,7 @@ exports.mail = (req, res) => {
   const subject = "NewSku Subsciption";
   const email = req.params.email;
   const message = "Subscription successfully activated";
-
+  const clientname = req.params.ClientName;
   var transporter = nodemailer.createTransport({
     host: "smtp.123-reg.co.uk", // hostname
     secureConnection: true, // use SSL
@@ -37,6 +37,21 @@ exports.mail = (req, res) => {
           <script async custom-element="amp-anim" src="https://cdn.ampproject.org/v0/amp-anim-0.1.js"></script>
         </head>
         <body>
+        <div>
+          <img src="" alt="NewSku" style="height:60px; margin:10px auto 30px auto;"/>
+          <div>
+          Hi Steven,
+          Thanks for subcribing to our 7 day trial.
+          Your free trial will end on 23/3/21.
+          We will be intouch for your feedback and hopefully discuss
+          you joining on a full subscription.
+          If you need support please email us at: info@newsku.co.uk
+          Sincerely,
+          NewSku Team
+          Connect with us.
+          <dip>
+        <div>
+
         <p>Thank you for continuing your subscription with NewSku. Your new subscription end date is now < enter new date automatically > 
 
         You will be notified 2 weeks prior to subscription ending </p>
